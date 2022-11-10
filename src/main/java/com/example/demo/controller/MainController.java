218583e219dface568fc1d84358ee76c1c3de163
yejin25 <yjin9187@naver.com>
2022-11-10 10:03:34 +0900

    modify travis config

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 
 @Controller
@@ -10,13 +11,16 @@ import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 public class MainController {
 
     @RequestMapping("/")
-    public String main() throws InterruptedException {
+    public ModelAndView indexPage() {
 
         var regionProvider = DefaultAwsRegionProviderChain.builder().build();
         var region = regionProvider.getRegion();
 
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("currentRG", region);
+
         System.out.println(region);
 
-        return "index";
+        return modelAndView;
     }
 }

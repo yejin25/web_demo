073c6aeb9b85f2eee7df274528c38e393d3d49a7
yejin25 <yjin9187@naver.com>
2022-11-10 09:43:56 +0900

    config travis

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 
 @Controller
 @RequiredArgsConstructor
@@ -10,6 +11,12 @@ public class MainController {
 
     @RequestMapping("/")
     public String main() throws InterruptedException {
+
+        var regionProvider = DefaultAwsRegionProviderChain.builder().build();
+        var region = regionProvider.getRegion();
+
+        System.out.println(region);
+
         return "index";
     }
 }
